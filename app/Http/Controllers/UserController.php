9e1a197c26ae2bf0a5f6a61521972426ec2c5eb4
SaraBarrett <sara.c.mon@gmail.com>
2024-09-12 11:47:51 +0100

    data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,16 +2,65 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Ramsey\Uuid\Guid\Guid;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function addUser(){
+
+        //insert
+        // DB::table('users')
+        // ->insert([
+        //     'name' => 'Sara',
+        //     'email' => 'Sara111@gmail.com',
+        //     'password' => 'Saraaaaa'
+        // ]);
+
+        //update
+        // DB::table('users')
+        // ->insert([
+        //     'name' => 'Sara',
+        //     'email' => 'Sara111@gmail.com',
+        //     'password' => 'Saraaaaa'
+        // ]);
+
+        // DB::table('tasks')
+        // ->where('id', 2)
+        // ->update([
+        //     'description' => 'operações à base de dados'
+        // ]);
+
+        DB::table('users')
+        ->updateOrInsert([
+            'email' => 'Sara8@gmail.com'
+        ],
+        [
+            'name' => 'Sara8',
+            'password' => 'Sara18',
+            'updated_at' => now(),
+        ]);
+
+        // dd('update ok');
+
+
         return view('users.add_user');
     }
 
     public function allUsers(){
-        return view('users.show_users');
+        $users = $this->getUsers();
+        $myUser = DB::table('users')->where('name', 'Sara')->first();
+
+        return view('users.show_users', compact('users', 'myUser'));
+    }
+
+    protected function getUsers(){
+        $users = DB::table('users')
+        ->where('name', 'Sara8')
+        ->get();
+
+        //dd(  $users);
+        return $users;
     }
 }
--- resources/views/users/show_users.blade.php
+++ resources/views/users/show_users.blade.php
@@ -1,4 +1,28 @@
 @extends('layouts.main')
 @section('content')
-    <h1>Olá, aqui vais ver uma lista de users!</h1>
+    <h5>O gestor pedagógico da turma é o {{ $myUser->name }} e o email dele é {{ $myUser->email }}</h5>
+    <h1>Olá, aqui vais ver uma lista de users! </h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Password</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <th scope="row">{{ $user->id }}</th>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->password }}</td>
+                </tr>
+            @endforeach
+
+
+
+        </tbody>
+    </table>
 @endsection
